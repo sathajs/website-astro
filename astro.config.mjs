3ca3d684423e9a1c6621712ff4ce78032bb4b6a0
Siddhesh Mangela <sidthecool007@gmail.com>
2021-12-20 15:21:09 +0530

    sitemap generator added

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,4 +10,8 @@
 export default /** @type {import('astro').AstroUserConfig} */ ({
   // Comment out "renderers: []" to enable Astro's default component support.
   renderers: ['@astrojs/renderer-svelte'],
+  buildOptions: {
+    sitemap: true,
+    site: 'https://satha.netlify.app/',
+  },
 });
